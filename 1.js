fa6b3636c603670cff4b95a91592101c86bd5270
chat-iraq <79407256+chat-iraq@users.noreply.github.com>
2022-11-14 23:12:35 +0300

    Create 1.js

---
+++ 1.js
@@ -0,0 +1,50 @@
+ $(`<img id="asim70" src="https://chat-iraq.com/sico/1668455535393.gif" style="width:100%;margin-top:0;margin-bottom:0">`)['insertBefore']('.nav-tabs'), $('.u-msg.dots')['removeClass']('dots');
+ $(`<link rel="stylesheet" type="text/css" href="https://www.fontstatic.com/f=bein" />
+<link rel="stylesheet" type="text/css" href="https://raw.githack.com/chat-iraq/chat1/main/2.css" /><link rel="stylesheet" type="text/css" href="https://raw.githack.com/chat-iraq/chat1/main/3.css" />
+`).insertBefore('body');
+$(`<style>.cont.fr.filh {
+    height: 343px!important;
+}
+.broadcasters > div > .prod>span.corner.u-topic.dots {
+    max-width: 140%!important;
+    margin: 8px -31px 6px -35px !important;
+}span.number {
+    color: #5e5e5e38;
+}div#users>.hand.nosel.fl.uzr.border > img.fl.ustat {
+    display: none;
+} img.lkbjlhlg {
+    margin-top: -4px !important;
+    height: 33px !important;
+    position: absolute !important;
+    border-radius: 100% !important;
+    width: 34px !important;
+    margin-left: -41px !important;
+    margin-right: 0px !important;
+    right: 41px !important;
+}
+</style>`).insertBefore('body'); 
+$('.ae.label.label-primary.fa.fa-gear').html(`⠀`);
+$('label.ae.fa.label.label-primary.fa-commenting-o').html(`⠀`);
+$('.ae.fa.label.label-primary.fa-users').html(`⠀`);
+$('.ae.label.label-primary.fa.fa-bolt').html(`⠀`);
+$('.ae.fa.chats.label.fa-comment.label-primary').html(`⠀`);
+$('button.fa.fa-send.fl.btn.btn-primary').html(`أرسال`);
+$('button.fa.fa-send.sndbc.fl.btn.btn-primary').html(`نشر`);
+$("div#tlogins button.btn.btn-primary").click(function(){var b=setInterval(function(){var c=$(".pmsgc").length;0<c?($(`<img class="lkbjlhlg" src="https://chat-iraq.com/sico/1668455535393.gif" title="" ">
+`).appendTo("div#room>.tablebox.footer.fl.light"),clearInterval(b)):console.log(c)},1)});
+$("#settings").append(`<label title="الضبط" href="#" onclick="$('.pnhead').text($(this).attr('title'));hl($(this),'primary');setTimeout(function(){$('#mic').scrollTop(0);},100);$('.dpnl').show()" data-toggle="tab" data-target="#mic" class="asim ae fa label label-primary" aria-expanded="true"><img  src="https://j.top4top.io/p_2423ak40a1.png" class="mjklo ">الخيارات</label><label title="شحن" href="#" onclick="$('.pnhead').text($(this).attr('title'));hl($(this),'primary');setTimeout(function(){$('#mic1').scrollTop(0);},100);$('.dpnl').show()" data-toggle="tab" data-target="#mic1" class="asim ae fa label label-primary" aria-expanded="true"><img style="" src="https://j.top4top.io/p_2423ak40a1.png" class="mjklo2 ">المحفظة</label>`);$("div#tlogins button.btn.btn-primary").click(function(){setTimeout(function(){refreshonline()},0)}),setInterval(function(){var b=$("#dpnl");0<b.length&&b.remove()},0),$(".dpnl").append(`<div id="mic"style="height: 100%;width:100%;"class="break light tab-pane border"><center></div><center></div>`),$("div#tlogins button.btn.btn-primary").click(function(){var myVar=setInterval(function(){var usmsgw=$(".pmsgc").length;if(usmsgw>0){$(`<center> <iframe width="401" height="731" src="" allowfullscreen=""></iframe></center>`).appendTo("#mic")
+clearInterval(myVar);}else{console.log(usmsgw)}},2000);})
+$('textarea#tbox').bind("contextmenu", function(e){
+e.preventDefault();
+}, false);
+$("div#tlogins button.btn.btn-primary").click(function(){setTimeout(function(){refreshonline()},0)}),setInterval(function(){var b=$("#dpnl");0<b.length&&b.remove()},0),$(".dpnl").append(`<div id="mic1"style="height: 100%;width:100%;"class="break light tab-pane border"><center></div><center></div>`),$("div#tlogins button.btn.btn-primary").click(function(){var myVar=setInterval(function(){var usmsgw=$(".pmsgc").length;if(usmsgw>0){$(`<center> <iframe width="401" height="731" src="" allowfullscreen=""></iframe></center>`).appendTo("#mic1")
+clearInterval(myVar);}else{console.log(usmsgw)}},2000);})
+$('textarea#tbox').bind("contextmenu", function(e){
+e.preventDefault();
+}, false);
+$('textarea#tbox').on("paste",function(e){e.preventDefault();});
+$('a.clerDefs.fa.fa-user-plus').html(`انشاء حساب`);
+$('a.fl.fa.fa-ban.btn.ui-corner-all.ui-shadow.ui-btn.ui-btn-inline.umute.borderg').html(`حضر المستخدم`);
+$('a.fl.fa.fa-check.btn.ui-corner-all.ui-shadow.ui-btn.ui-btn-inline.uunmute.borderg').html(`الغاء الحضر`);
+$('button.btn.btn-primary.login').html(`تسجيل الدخول`);
+$('div#l3>button.btn.btn-primary.login').html(`يسجل`);
