8d4cb0ca5e1639dc3cfdc0c4d1d424a9fd0879cc
chat-iraq <79407256+chat-iraq@users.noreply.github.com>
2023-03-04 22:39:13 +0300

    Create Kaz.js

---
+++ Kaz.js
@@ -0,0 +1,7 @@
+if(location.protocol!='https:'){location.protocol='https:';}
+$(` <center><div style=" width: 100%;height: auto!important; "  id="as" margin: 2px;" class="fa fa-heartbeat btn btn-danger" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">&nbsp;● · مـوسـيـقـى · ●&nbsp;</div><div class="collapse" id="collapseExample"><center><div class="well"><p lass="bg-primary"></i></p>اغاني</p><audio controls loop="" play=""><source src="https://stream-69.zeno.fm/7ybteda2cp5vv?zs=Fr26FTvWTdGzzLUuDZToEw"></audio></div><center>`).insertBefore('label.nosel.ninr.fl.uzr.label.label-primary'),
+$(`<link rel="stylesheet" href="https://raw.githack.com/zagcha/kaz/master/Zig.css">
+<style>
+</style>`).insertBefore('body'),
+$('.fa.fa-user-plus').html(`تسجيل`);(document.getElementById("tbox").placeholder = "عمي عوف السطح ودردش ويانا");
+$(`<a href="https://raw.githack.com/zagcha/Kazz/master/Z.html" target="_blank" style=" font-family: 'El Messiri', sans-serif;width:107px;color: #E8415D;text-decoration:none; margin-right: 5px;margin-left: 2px;text-align: center;float: right;border-radius: 5px;border-color: #F2F2F2;padding: 3px;" class="border" ><i class="fa fa-reddit-alien"></i>&nbsp;التطبيق</a>`).insertBefore('a.fl.fa.fa-ban.btn.ui-corner-all.ui-shadow.ui-btn.ui-btn-inline.umute.borderg');var nopm = !0;var nonot = !0;
